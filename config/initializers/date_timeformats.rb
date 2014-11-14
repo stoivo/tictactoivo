b4de214b11a452efc2b46ad2563bd36f2db20fc4
Simon <simon.toivo@telhaug.no>
2014-11-14 22:47:37 +0100

    done

---
+++ config/initializers/date_timeformats.rb
@@ -0,0 +1,8 @@
+Date::DATE_FORMATS[:default] = '%Y-%m-%d'
+# Date::DATE_FORMATS[:default] = '%m/%d/%Y'
+
+# if you want to change the format of Time display then add the line below
+Time::DATE_FORMATS[:default]= '%Y-%m-%d %H:%M:%S'
+
+# if you want to change the DB date format.
+Time::DATE_FORMATS[:db]= '%Y-%m-%d %H:%M:%S'
