48ab058dab5acfc4b0645c5cbc97590dc8f58664
Simon <simon.toivo@telhaug.no>
2014-11-20 22:42:21 +0100

    update user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,8 +20,15 @@ class UsersController < ApplicationController
   end
 
   def update
-    foo =(user_params :update)
-
+    respond_to do |format|
+      if @user.update(user_params :update)
+        format.html { redirect_to games_path, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def set_user
